fbc91d29037bfe3ffff25a64841e68d712f39374
Gustavo Souza <gtssouza94@gmail.com>
2020-12-01 15:42:37 -0300

    Configurando a conexão com o banco de dados MongoDB

---
+++ config/dbConn.js
@@ -0,0 +1,21 @@
+//importando o mongodb
+var mongo = require('mongodb');
+
+var connMongoDB = function(){
+    console.log('Entrou na função de conexão');
+    var db = new mongo.Db(
+        'got',
+        new mongo.Server(
+            'localhost',//string contendo o endereço do servidor
+            27017,//porta de conexao
+            {}
+        ),
+        {}
+    );
+    return db;
+}
+
+module.exports = function(){
+    
+    return connMongoDB;
+}
