a2c73ec58f1e6ce1d1f2a2275d2b99e60425b1eb
Gustavo Souza <gtssouza94@gmail.com>
2020-12-01 11:28:07 -0300

    Recebendo e validando dados na tela de cadastro

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,21 @@
 module.exports.cadastro = function(application, req,res){
-    res.render('cadastro');
+    res.render('cadastro', {validacao: {}, dadosForm: {}});
+}
+
+module.exports.cadastrar = function(application, req, res){
+    var dadosForm = req.body;
+
+    req.assert('nome','Nome não pode ser vazio').notEmpty();
+    req.assert('usuario','Usuario não pode ser vazio').notEmpty();
+    req.assert('senha','Senha não pode ser vazio').notEmpty();
+    req.assert('casa','Casa não pode ser vazio').notEmpty();
+
+    var error = req.validationErrors();
+
+    if(error){
+        res.render('cadastro', {validacao: error, dadosForm: dadosForm});
+        return;
+    }
+
+    res.send('Podemos cadastrar');
 }
